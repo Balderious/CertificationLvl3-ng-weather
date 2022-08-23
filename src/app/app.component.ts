e66fefb3eda0856323e3625e20fcc4b3a9b060d0
Ludovic Saufnay <ludovic.saufnay@gmail.com>
2022-08-24 00:22:03 +0200

    Push all project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import {SwPush, SwUpdate} from '@angular/service-worker';
+import {interval} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+
+    constructor() {
+
+    }
 }
